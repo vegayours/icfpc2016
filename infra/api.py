23febb1fe1ae90823fad96ef0bf688e76b8fde37
Aleksandr Likhanov <vegayours@gmail.com>
2016-08-05 17:40:50 +0500

    fetch / solve subcommands

--- infra/api.py
+++ infra/api.py
@@ -1,16 +1,12 @@
 from ratelimiter import RateLimiter
-import datetime
-import os
-import json
-
 import requests
-
 
 API_KEY = '174-bb96326ac744ca6ca3161cc6778cff85'
 
 HELLO_ENDPOINT = 'http://2016sv.icfpcontest.org/api/hello'
 LIST_ENDPOINT = 'http://2016sv.icfpcontest.org/api/snapshot/list'
 BLOB_ENDPOINT = 'http://2016sv.icfpcontest.org/api/blob/{}'
+SOLUTION_ENDPOINT = 'http://2016sv.icfpcontest.org/api/solution/submit'
 
 hour_limiter = RateLimiter(max_calls=1000, period=60*60)
 second_limiter = RateLimiter(max_calls=1, period=1.1)
@@ -21,7 +17,6 @@ def api_session():
             session = requests.Session()
             session.headers.update({'X-API-Key': API_KEY})
             return session
-
 
 def check_status(resp):
     if resp.status_code >= 500 and resp.status_code < 600:
@@ -65,23 +60,9 @@ def fetch_blob(ref):
     check_status(resp)
     return resp.text
 
-def fetch_problems(workdir):
-    snapshot_ref = latest_snapshot()
-    datetime_label = datetime.datetime.utcfromtimestamp(snapshot_ref['snapshot_time'])
-    snapshot_name = os.path.join(workdir, 'snapshot_{}_{}.json'.format(datetime_label, snapshot_ref['snapshot_hash']))
-    snapshot = fetch_blob(snapshot_ref['snapshot_hash'])
-    with open(snapshot_name, 'w') as f:
-        f.write(snapshot)
-    for p in json.loads(snapshot)['problems']:
-        problem_name = os.path.join(workdir, 'problem_{problem_id:04}_{problem_size}.json'.format(**p))
-        p['spec'] = fetch_blob(p['problem_spec_hash'])
-        print 'Problem: {}'.format(problem_name)
-        with open(problem_name, 'w') as f:
-            f.write(json.dumps(p))
-
-
-
-if __name__ == '__main__':
-    workdir = 'problems'
-    os.mkdir(workdir)
-    fetch_problems(workdir)
+@api_retry
+def push_solution(problem, solution):
+    payload = {'problem_id': problem['problem_id'], 'solution_spec': solution}
+    resp = api_session().post(SOLUTION_ENDPOINT, data=payload)
+    check_status(resp)
+    return resp.json()
---
+++ infra/main.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python2
+
+import os
+from os import path
+from argparse import ArgumentParser
+import api
+import json
+
+def ensure_dir(d):
+    if not os.path.exists(d):
+        os.mkdir(d)
+
+def fetch_problems(args):
+    workdir = args.workdir
+    ensure_dir(workdir)
+
+    snapshot_ref = api.latest_snapshot()
+    snapshot_name = os.path.join(workdir, 'snapshot.json')
+    snapshot = api.fetch_blob(snapshot_ref['snapshot_hash'])
+    with open(snapshot_name, 'w') as f:
+        f.write(snapshot)
+
+    for p in json.loads(snapshot)['problems']:
+        problem_dir = os.path.join(workdir, 'problem_{:05}'.format(p['problem_id']))
+        ensure_dir(problem_dir)
+        problem_name = os.path.join(problem_dir, 'problem.json')
+        spec_name = os.path.join(problem_dir, 'spec.txt')
+        spec = api.fetch_blob(p['problem_spec_hash'])
+        p['spec'] = spec
+        with open(spec_name, 'w') as f:
+            f.write(spec)
+        with open(problem_name, 'w') as f:
+            f.write(json.dumps(p))
+        print 'Problem: {}'.format(problem_name)
+
+def push_solution(args):
+    problem_file = path.join(args.problem_dir, 'problem.json')
+    if not path.exists(args.problem_dir) or not path.exists(problem_file):
+        raise 'Invalid problem path: {}'.format(args.problem_dir)
+    problem = json.load(open(problem_file))
+    solution = open(args.solution_file).read()
+    solution_dir = path.join(args.problem_dir, args.name)
+    ensure_dir(solution_dir)
+
+    resp = api.push_solution(problem, solution)
+    print resp
+
+    with open(path.join(solution_dir, 'solution.txt'), 'w') as f:
+        f.write(solution)
+
+    with open(path.join(solution_dir, 'checker.json'), 'w') as f:
+        json.dump(resp, f)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    fetch = subparsers.add_parser('fetch')
+    fetch.add_argument('workdir')
+    fetch.set_defaults(func=fetch_problems)
+
+    solve = subparsers.add_parser('solve')
+    solve.add_argument('problem_dir')
+    solve.add_argument('solution_file')
+    solve.add_argument('--name', default='manual')
+    solve.set_defaults(func=push_solution)
+
+    args = parser.parse_args()
+    args.func(args)
